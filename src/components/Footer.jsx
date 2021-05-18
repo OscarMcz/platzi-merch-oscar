ef151cbbc19988520bdaeb0c7abd91cd924d9c21
Oscar Martinez <oscar1793@hotmail.com>
2021-05-18 11:58:19 -0500

    [add] firebase hosting

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/components/Footer.css'
+
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <p className="Footer-tittle">PlatziConf Merch</p>
+            <p className="Footer-copy">
+                Todos los Izquierdos Reservados
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
